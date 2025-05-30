e4b7945b3ee0a4fb5a958af9d3a5c9d3c8a2c748
Hrushikeshj26 <hrushij92@gmail.com>
2025-05-30 20:30:40 +0530

    final stage

--- Practice03/MovieSpace/src/App.jsx
+++ Practice03/MovieSpace/src/App.jsx
@@ -1,30 +1,60 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './Card'
 
+
+const API_URL = "http://www.omdbapi.com?apikey=b169cf0";
+
 function App() {
-  const [count, setCount] = useState(0)
-  let len=8;
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    if (data.Search) {
+      setMovies(data.Search);
+    } else {
+      setMovies([]);
+    }
+  }
+
+  useEffect(() => {
+    searchMovies("superman");
+  },[]);
+
   return (
     <div className='w-full h-screen flex flex-col items-center bg-black text-white'>
       <h1 className='mt-8 text-5xl font-bold'>MovieSpace</h1>
       <div className='flex items-center justify-center gap-4 mt-4'>
         <input 
           type="text" 
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search for movies..."
           className="py-2 rounded-xl px-36 bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
          />
         <button 
+          onClick={() => searchMovies(searchTerm)}
           className="px-8 rounded-xl py-2 bg-blue-600 rounded hover:bg-blue-700 transition-colors"> Search </button>
       </div>
-      <div className='bg-gray-950 rounded-2xl w-full h-auto flex items-center justify-center m-8'>
+      <div className='bg-gray-500 rounded-2xl w-full h-auto flex items-center justify-center m-8'>
         <div>
-          <div className=' grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-6 p-8'>
-            {Array.from({ length: len }).map((_, index) => (
-              <Card  />
-            ))}
+          <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5 gap-6 p-8'>
+            {movies.length > 0 ? (
+              movies.map((movie) => (
+                <Card 
+                  key={movie.imdbID}
+                  movie={movie}
+                />
+              ))
+            ) : (
+              <div className='h-screen text-xl text-white'>No movies found</div>
+            )}
           </div>
         </div>
       </div>
--- Practice03/MovieSpace/src/Card.jsx
+++ Practice03/MovieSpace/src/Card.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
 
-function Card() {
+function Card({ movie: {Title, Poster, Year,imdbID } }) {
   return (
     <div>
-        <div className="bg-gray-800 rounded-lg p-6 w-80">
+        <div className="bg-gray-800 rounded-lg p-6 w-80" key={{imdbID}}>
             <img 
-            src="https://via.placeholder.com/150" 
+            src={Poster || 'https://via.placeholder.com/150'} 
             alt="Movie Poster" 
-            className="w-full h-48 object-cover rounded-md mb-4"
+            className="w-full h-fit object-cover rounded-md mb-4"
             />
-            <h2 className="text-xl font-bold mb-2">Movie Title</h2>
-            <p className="text-gray-400 mb-4">Release Date: 2023</p>
-            <p className="text-gray-300 mb-4">This is a brief description of the movie. It gives an overview of the plot and main characters.</p>
-            <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors">
-            Watch Now
-            </button>
+            <h2 className="text-l font-bold mb-2">{Title}</h2>
+            <p className="text-gray-400 mb-4">Release Date: {Year}</p>
         </div>
     </div>
   )
